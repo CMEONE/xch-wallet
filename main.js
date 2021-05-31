0618b3aeb3740498d502834317262e8526a10c50
CMEONE <contact@kabirwearsato.ga>
2021-05-31 16:08:16 -0700

    Basic styling

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ const path = require('path');
 
 const createWindow = () => {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
+		width: 1000,
+		height: 800,
 		webPreferences: {
 			preload: path.join(__dirname, 'preload.js')
-		}
+		},
+		title: "XCH Wallet"
 	});
 
 	win.loadFile('index.html');
