9f616d4635c13d984b07412c28846573bd26d4f8
CMEONE <contact@kabirwearsato.ga>
2021-05-31 17:20:00 -0700

    Connect to wallet

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
+
+let win;
+
 const createWindow = () => {
-	const win = new BrowserWindow({
+	win = new BrowserWindow({
 		width: 1000,
 		height: 800,
 		webPreferences: {
@@ -16,16 +19,26 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
 	createWindow();
-
 	app.on('activate', function () {
 		if(BrowserWindow.getAllWindows().length === 0) {
 			createWindow();
 		}
-	})
+	});
 });
 
 app.on('window-all-closed', () => {
 	if(process.platform !== 'darwin') {
 		app.quit();
+	}
+});
+
+const { Wallet } = require("chia-client");
+const wallet = new Wallet();
+
+ipcMain.on("wallet", (event, args) => {
+	if(args.command == "getPublicKeys") {
+		wallet.getPublicKeys().then((keys) => {
+			win.webContents.send(`response-wallet-${args.token}`, keys);
+		});
 	}
 });
--- preload.js
+++ preload.js
@@ -1,3 +1,55 @@
+const cli = require('child_process').exec;
+const fs = require('fs');
+const { ipcRenderer } = require('electron');
+
+function Wallet() {
+	const sendCommand = (command, args) => {
+		return new Promise(async (resolve, reject) => {
+			let token = Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
+			ipcRenderer.on(`response-wallet-${token}`, (event, response) => resolve(response));
+			ipcRenderer.send("wallet", {
+				command,
+				args,
+				token
+			});
+		});
+	}
+
+	this.getPublicKeys = () => {
+		return sendCommand("getPublicKeys", []);
+	}
+}
+
+const execute = (command) => {
+	return new Promise(async (resolve, reject) => {
+		cli(command, (error, stdout, stderr) => {
+	    	if(stderr != "") {
+	    		reject(stderr);
+	    	} else {
+		        resolve(stdout);
+		    }
+	    });
+	});
+};
+
+let chia = "chia";
+if(process.platform === 'darwin') {
+	chia = "/Applications/Chia.app/Contents/Resources/app.asar.unpacked/daemon/chia";
+} else if(process.platform === "win32") {
+	chia = fs.readdirSync("~\\AppData\\Local\\chia-blockchain\\").filter(f => f.startsWith("app-"))[0] + "\\resources\\app.asar.unpacked\\daemon\\chia.exe";
+} else if(process.platform === "linux") {
+	chia = "/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia";
+}
+
+const wallet = new Wallet();
+
 window.addEventListener('DOMContentLoaded', () => {
-	
+	execute(`${chia} init && ${chia} start wallet`).then((res) => {
+		document.getElementById("loader").innerHTML = `<p>Connecting to Wallet...</p>`;
+		wallet.getPublicKeys().then((keys) => {
+			console.log(keys);
+		});
+	}).catch((err) => {
+		throw err;
+	});
 });
